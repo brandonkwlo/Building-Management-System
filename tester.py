a73157c5f54531b099eb52bae4cebc272d730673
brandonkwlo <39466438+brandonkwlo@users.noreply.github.com>
2023-06-13 10:54:02 -0700

    Changes made

--- tester.py
+++ tester.py
@@ -1,4 +1,4 @@
-### Import Python Modules ###
+# Libraries
 import threading
 import RPi.GPIO as GPIO
 from datetime import datetime, timedelta
@@ -10,11 +10,10 @@ from PCF8574 import PCF8574_GPIO
 from Adafruit_LCD1602 import Adafruit_CharLCD
 import Freenove_DHT as DHT
 
-### Pin Numbering Declaration (setup channel mode of the Pi to Board values) ###
-GPIO.setwarnings(False)		# to disable warnings
+# Setup
+GPIO.setwarnings(False)		
 GPIO.setmode(GPIO.BOARD)
 
-### Set GPIO pins (for inputs and outputs) and all setups needed based on assignment description ###
 LED_G = 40
 LED_R = 32 
 LED_B = 33
@@ -30,9 +29,14 @@ hvac_update = False	# HVAC status change displayed or not
 hvac_msg = 'OFF '	# HVAC status (off by default, other two are AC and HEAT)
 terminated = False
 humidity = None		# Humidity (Will be updated by CIMIS data)
-curr_temp = 0		# current temperature (Will be updated by DHT)
+weather_index = 0		# current temperature (Will be updated by DHT)
 des_temp = 75		# desired temperature (75 degrees Farenheit by default)
 fname = 'log.txt'	# file name
+
+# CIMIS URL Parameters
+appKey = '94c69e9a-5942-4315-9277-d758af3202ec'
+target = '75'
+items = 'hly-rel-hum'
 
 GPIO.setup(LED_G, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(LED_R, GPIO.OUT, initial=GPIO.LOW)
@@ -67,11 +71,8 @@ def get_hum(hr, curr):
 	else:
 		date_str = curr.strftime('%Y-%m-%d')
 		
-	req_url ='http://et.water.ca.gov/api/data?appKey=94c69e9a-5942-4315-9277-d758af3202ec&targets=75&startDate='
-	req_url = req_url + date_str
-	req_url = req_url + '&endDate='
-	req_url = req_url + date_str
-	req_url = req_url + '&dataItems=hly-rel-hum'
+	req_url ='http://et.water.ca.gov/api/data?appKey='+appKey+'&targets='+target+'&startDate='
+	req_url = req_url+date_str+'&endDate='+date_str+'&dataItems='+items
 	print('[CIMIS] URL ready')
 	content = None
 	data = None
@@ -108,12 +109,12 @@ def get_hum(hr, curr):
 def check_temp():
 	global hvac_msg
 	global hvac_update
-	global curr_temp
+	global weather_index
 	global des_temp
 	global dw_status
 	global fname
 	old_msg = hvac_msg
-	diff = curr_temp - des_temp
+	diff = weather_index - des_temp
 	if((diff >= 3) and (dw_status)):
 		hvac_msg = 'AC  '
 		GPIO.output(LED_B, GPIO.HIGH)
@@ -149,7 +150,7 @@ def hum_thread():
 	print('[Main] CIMIS Thread terminated')
 		
 def DHT_thread(lock):
-	global curr_temp
+	global weather_index
 	global des_temp
 	global humidity
 	global terminated
@@ -167,18 +168,12 @@ def DHT_thread(lock):
 			i = 0
 		else:
 			i += 1
-		# lock.acquire()
 		assert(humidity is not None)
-		# print(t_arr)
-		m = (sum(t_arr) / 3.0) * 1.8 + 32.0 + 0.05 * float(humidity)
-		curr_temp = int(m)
-		# lock.release()
+		m = (sum(t_arr) / 3.0) * 1.8 + 32.0 + 0.05 * float(humidity) #weather index calc
+		weather_index = int(m)
 		time.sleep(1)
 	print('[Main] DHT Thread terminated')
 
-# PIR Sensor Thread
-# Due to how this part is implemented, the overhead in updating the variables may result in a small delay when writing the change log
-# But the timing of the program is actually correct, which can be verified by the demo video of this project.
 def PIR_thread(lock):
 	count = 0
 	global l_status
@@ -190,7 +185,6 @@ def PIR_thread(lock):
 		if(GPIO.input(PIR_SENSOR) == GPIO.HIGH):
 			count = 0
 			GPIO.output(LED_G, True)
-			# lock.acquire()
 			old_status = l_status
 			l_status = True
 			print('[PIR] Movement detected')
@@ -200,13 +194,11 @@ def PIR_thread(lock):
 				time_str = time_str + ' LIGHTS ON\n'
 				f.write(time_str)
 				f.close()
-			# lock.release()
 		else:
 			count += 1
 			
 		if(count == 5):
 			GPIO.output(LED_G, False)
-			# lock.acquire()
 			old_status = l_status
 			l_status = False
 			print('[PIR] No movement detected in the past 10 seconds')
@@ -216,19 +208,18 @@ def PIR_thread(lock):
 				time_str = time_str + ' LIGHTS OFF\n'
 				f.write(time_str)
 				f.close()
-			# lock.release()
 			count = 0
 	print('[Main] PIR Thread terminated')
 
 def lcd_display():
-	global curr_temp
+	global weather_index
 	global des_temp
 	global dw_status
 	global hvac_msg
 	global l_status
 	global lcd
 	lcd.setCursor(0, 0)
-	lcd.message(str(curr_temp)+'/'+str(des_temp))
+	lcd.message(str(weather_index)+'/'+str(des_temp))
 	lcd.message('   D:')
 	if(dw_status):
 		lcd.message('SAFE \n')
@@ -248,7 +239,7 @@ def lcd_thread(lock):
 	global hvac_update
 	global dw_update
 	global dw_status
-	global curr_temp
+	global weather_index
 	global des_temp
 	global l_status
 	global terminated
@@ -256,9 +247,7 @@ def lcd_thread(lock):
 	old_status = l_status
 	old_des_t = des_temp
 	while(not terminated):
-		# lock.acquire()
 		check_temp()
-		# lock.release()
 		if(dw_update):
 			lcd.clear()
 			lcd.setCursor(0,0)
@@ -281,9 +270,7 @@ def lcd_thread(lock):
 			time.sleep(3)
 			lcd.clear()
 		else:
-			# lock.acquire()
 			lcd_display()
-			# lock.release()
 			time.sleep(0.1)
 	print('[Main] LCD Thread terminated')
 
@@ -321,33 +308,29 @@ def handle(pin):
 GPIO.add_event_detect(SECURE_BTN, GPIO.RISING, callback=handle, bouncetime=300)
 GPIO.add_event_detect(HEAT_BTN, GPIO.RISING, callback=handle, bouncetime=300)
 GPIO.add_event_detect(AC_BTN, GPIO.RISING, callback=handle, bouncetime=300)
+
 	
 if __name__ == '__main__':
 	try: 
-		print('[Main] BMS starts')
+		print('Starting....')
+		# line below prevents race conditions and ensures thread safety
 		lock = threading.Lock()
-		curr = datetime.now()
-		hr = curr.hour
-		print('[Main] CIMIS Thread Start')
+		print('CIMIS Thread Start')
 		t0 = threading.Thread(target=hum_thread)
-		# t0.daemon = True
 		t0.start()
 		while(humidity is None):
 			time.sleep(1)
-		print('[Main] Initializing PIR sensor, please allow about 1 minutes to set up')
+		print('Initializing PIR sensor....')
 		t1 = threading.Thread(target=DHT_thread, args=(lock,))
-		# t1.daemon = True
 		t1.start()
-		print('[Main] Waiting for initial temperature being calculated...')
+		print('Temperature being calculated....')
 		time.sleep(5)
-		print('[Main] Current temperature is ready')
+		print('Current temperature is ready')
 		t2 = threading.Thread(target=lcd_thread, args=(lock,))
-		# t2.daemon = True
 		t2.start()
 		time.sleep(55)
-		print('[Main] PIR is ready')
+		print('PIR is ready')
 		t3 = threading.Thread(target=PIR_thread, args=(lock,))
-		# t3.daemon = True
 		t3.start()
 		msg = input('[Main] Press <Enter> key to exit the program: \n')
 		terminated = True
@@ -358,7 +341,7 @@ if __name__ == '__main__':
 		mcp.output(3,0)
 		lcd.clear()
 		GPIO.cleanup()
-		print('BMS ends')
+		print('END')
 	except KeyboardInterrupt:
 		terminated = True
 		t0.join()
@@ -368,14 +351,5 @@ if __name__ == '__main__':
 		mcp.output(3,0)
 		lcd.clear()
 		GPIO.cleanup()
-		print('BMS ends')
-	except:
-		terminated = True
-		t0.join()
-		t1.join()
-		t2.join()
-		t3.join()
-		mcp.output(3,0)
-		lcd.clear()
-		GPIO.cleanup()
-		print('BMS ends')
+		print('END')
+
